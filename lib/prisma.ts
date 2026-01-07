de31764cba4914724c414dbce7490d0733513e84
kang`ethe <offline@kali.dkmuthunga.com>
2026-01-08 00:55:47 +0300

    test4

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import * as PrismaAll from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pkg from "pg";
 
+const { PrismaClient } = PrismaAll;
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_POOLING_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({
-  adapter,
+  adapter
 });
